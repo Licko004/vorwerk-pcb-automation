8dadb89e2f3d338458c560a7a836b81ce4692615
Licko004 <tim.licen@dewesoft.com>
2026-09-23 08:15:11 +0100

    contours drawn on image test succesfull

--- angle-pos-test.py
+++ angle-pos-test.py
@@ -8,7 +8,7 @@ import time
 from picamera2 import Picamera2
 picam2 = Picamera2()
 
-picam2.preview_configuration.main.size = (1280,720)
+picam2.preview_configuration.main.size = (1920,1080)
 picam2.preview_configuration.main.format = "RGB888"
 picam2.preview_configuration.align()
 picam2.configure("preview")
@@ -27,52 +27,39 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imwrite("grayscale.png", gray)
 
 # threshold the grayscale image
-ret1, thresh1 = cv2.threshold(gray,127,255,cv2.THRESH_TRUNC)
-ret2, thresh2 = cv2.threshold(gray,127,255,cv2.THRESH_TOZERO)
-ret3, thresh3 = cv2.threshold(gray,127,255,cv2.THRESH_TOZERO_INV)
-ret4, thresh4 = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
+# ret1, thresh1 = cv2.threshold(gray,127,255,cv2.THRESH_TRUNC)
+# ret2, thresh2 = cv2.threshold(gray,127,255,cv2.THRESH_TOZERO)
+# ret3, thresh3 = cv2.threshold(gray,127,255,cv2.THRESH_TOZERO_INV)
+ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
 
-cv2.imwrite("threshold-trunc.jpg", thresh1)
-cv2.imwrite("threshold-tozero.png", thresh2)
-cv2.imwrite("threshold-tozero-inv.png", thresh3)
-cv2.imwrite("threshold-binary.png", thresh4)
+cv2.imwrite("threshold-otsu.png", thresh)
+# cv2.imwrite("threshold-tozero.png", thresh2)
+# cv2.imwrite("threshold-tozero-inv.png", thresh3)
+# cv2.imwrite("threshold-binary.png", thresh4)
 
-im2, contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(thresh1, contours, -1, (0,255,0), 3)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contour_img = img.copy()
 
-cv2.imwrite("contours.png", thresh1)
+cv2.drawContours(contour_img, contours, -1, (0,255,0), 3)
 
-# # find outer contour
-# cntrs = cv2.findContours(thresh1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-# cntrs = cntrs[0] if len(cntrs) == 2 else cntrs[1]
+cv2.imwrite("contours.png", contour_img)
 
 # # get rotated rectangle from outer contour
-# rotrect = cv2.minAreaRect(cntrs[0])
-# box = cv2.boxPoints(rotrect)
-# box = np.int8(box)
+rotrect = cv2.minAreaRect(contours[0])
+box = cv2.boxPoints(rotrect)
+box = np.intp(box)
 
 # # draw rotated rectangle on copy of img as result
-# result = img.copy()
-# cv2.drawContours(result,[box],0,(0,0,255),2)
+result = img.copy()
+cv2.drawContours(result,[box],0,(0,255,0),3)
+
+cv2.imwrite("box.png", result)
 
 # # get angle from rotated rectangle
-# angle = rotrect[-1]
+angle = rotrect[-1]
 
-# # from https://www.pyimagesearch.com/2017/02/20/text-skew-correction-opencv-python/
-# # the `cv2.minAreaRect` function returns values in the
-# # range [-90, 0); as the rectangle rotates clockwise the
-# # returned angle trends to 0 -- in this special case we
-# # need to add 90 degrees to the angle
-# # if angle < -45:
-# #     angle = -(90 + angle)
- 
-# # # otherwise, just take the inverse of the angle to make
-# # # it positive
-# # else:
-# #     angle = -angle
-
-# print(angle,"deg")
+print(angle,"deg")
 
 # # write result to disk
 # cv2.imwrite("wing2_rotrect.png", result)
