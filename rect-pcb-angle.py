bb864642e8a92019347378e76790cc068cacb45f
Licko004 <tim.licen@dewesoft.com>
2026-09-24 12:10:30 +0100

    Line drawn according to longer edge of rectangle, sign issue!

--- rect-pcb-angle.py
+++ rect-pcb-angle.py
@@ -7,6 +7,7 @@ import numpy as np
 import time
 import board
 import neopixel
+import math
 
 # Configure the setup
 # PIXEL_PIN = board.D18       # pin that the NeoPixel ring is connected to
@@ -28,21 +29,19 @@ picam2.preview_configuration.align()
 picam2.configure("preview")
 
 picam2.start()
-time.sleep(2)
 img  = picam2.capture_file("test.png")
-time.sleep(1)
 
 img = cv2.imread("test.png")
-hh, ww, cc = img.shape
+
 
 # convert to gray
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imwrite("grayscale-noise.png", gray)
+# cv2.imwrite("grayscale-noise.png", gray)
 
 #Thresholding the grayscale image
 ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-cv2.imwrite("threshold-otsu.png", thresh)
+# cv2.imwrite("threshold-otsu.png", thresh)
 
 # Source - https://stackoverflow.com/a/59238613
 # Posted by chamith mawela
@@ -56,11 +55,12 @@ cv2.imwrite("contours-unfiltered.png", contour_unfiltered)
 
 cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 rect_areas = []
-for c in cnts:
+for c in cnts: # exclude largest found contour
     (x, y, w, h) = cv2.boundingRect(c)
     rect_areas.append(w * h)
     
-avg_area = np.mean(rect_areas)
+avg_area = np.average(rect_areas)
+print("avg area:", avg_area)
 rect_drawn = img.copy()
 
 for c in cnts:
@@ -72,7 +72,8 @@ big_cnts = []
 for c in cnts:
     (x, y, w, h) = cv2.boundingRect(c)
     cnt_area = w * h
-    if cnt_area > 0.20 * avg_area and cnt_area < 0.40 * avg_area:
+    print("Unfiltered areas:", cnt_area)
+    if cnt_area > 0.30 * avg_area and cnt_area < 0.80 * avg_area:
         # thresh_filtered[y:y + h, x:x + w] = 0
         big_cnts.append(c)
         
@@ -90,14 +91,45 @@ for c in big_cnts:
     cy = y + h/2
     cx_int = int(cx)
     cy_int = int(cy)
-    print(cx,",",cy)
+    print("Rectangle center positon: ", cx,",",cy)
     points_img = cv2.circle(contour_filtered, (cx_int,cy_int), 5, color = (0,0,255), thickness = -1)
-    print(fil_rect_area)
-    if fil_rect_area > 4000 and fil_rect_area < 7000: #filtering by pixel area of contours
+    print("Rect_area",fil_rect_area)
+    if fil_rect_area > 4000 and fil_rect_area < 8000: #filtering by pixel area of contours
         # cv2.rectangle(contour_filtered, (x, y), (x + w, y + h), (0, 0, 255), 2) # Draws rectangle around pcb, also including spikes etc
         rotrect = cv2.minAreaRect(c)
         box = cv2.boxPoints(rotrect)
         box = np.int32(box)
+        # Draw line at rotrect angle of length 18mm
+        line_length = 17
+        line_angle_def = rotrect[-1]
+
+        # find the longer edge of the rectangle, to compute the angle
+        edge1 = box[1] - box[0]
+        edge2 = box[2] - box[1]
+        
+        if np.linalg.norm(edge1) > np.linalg.norm(edge2):
+            long_edge = edge1
+        else:
+            long_edge = edge2
+        
+        
+        long_edge_angle = math.degrees(math.atan2(long_edge[1], long_edge[0]))
+        line_angle = long_edge_angle + 90   # perpendicular to the long edge
+        
+        DPI = 6.279251163812498 # gathered from DPI_calibration.py
+        pixel_legth = line_length*DPI
+        print("rectangle angle: ", line_angle_def)
+        
+        line_angle = long_edge_angle + 90
+        print("line angle: ", line_angle)
+        
+        angle_radians = math.radians(line_angle)
+        
+        x2 = int(cx + pixel_legth * math.cos(angle_radians))
+        y2 = int(cy + pixel_legth * math.sin(angle_radians))
+        
+        cv2.line(contour_filtered, (cx_int, cy_int), (x2, y2), color = (0,255,0), thickness = 3)
+        
         cv2.drawContours(contour_filtered, [box], 0, (0, 0, 255), 2)      
 
     
